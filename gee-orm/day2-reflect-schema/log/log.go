087bd484b14670e97404e7f9770697ebf7c0d70b
hpfish <gaoxinyu17@gmail.com>
2024-04-02 07:57:15 +0800

    geeorm day2

---
+++ gee-orm/day2-reflect-schema/log/log.go
@@ -0,0 +1,46 @@
+package log
+
+import (
+	"io"
+	"log"
+	"os"
+	"sync"
+)
+
+const (
+	InfoLevel = iota
+	ErrorLevel
+	Disabled
+)
+
+var (
+	errorLog = log.New(os.Stdout, "\033[31m[error]\033[0m", log.LstdFlags|log.Lshortfile)
+	infoLog  = log.New(os.Stdout, "\033[34m[info]\034[0m", log.LstdFlags|log.Lshortfile)
+	loggers  = []*log.Logger{errorLog, infoLog}
+	mu       sync.Mutex
+)
+
+var (
+	Error  = errorLog.Println
+	Errorf = errorLog.Printf
+	Info   = infoLog.Println
+	Infof  = infoLog.Printf
+)
+
+func SetLevel(level int) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	for _, logger := range loggers {
+		logger.SetOutput(os.Stdout)
+	}
+
+	if ErrorLevel < level {
+		errorLog.SetOutput(io.Discard)
+	}
+
+	if InfoLevel < level {
+		infoLog.SetOutput(io.Discard)
+	}
+
+}
